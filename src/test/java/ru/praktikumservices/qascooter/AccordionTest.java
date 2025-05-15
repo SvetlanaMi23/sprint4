4ff69cde7c4887b6eda28bdb28dec4c374b620eb
Svetlana Mishchenko <sveeetil@yandex.ru>
2025-05-15 22:16:29 +0300

    Исправлено замечание - локаторы вынесены в MainPage из AccordionTest.

--- src/test/java/ru/praktikumservices/qascooter/AccordionTest.java
+++ src/test/java/ru/praktikumservices/qascooter/AccordionTest.java
@@ -10,43 +10,43 @@ import ru.praktikumservices.qascooter.pom.MainPage;
 @RunWith(Parameterized.class) // Аннотация для параметризованных тестов
 public class AccordionTest extends BasePageTest {
     private static final String url = "https://qa-scooter.praktikum-services.ru/";
-    private final String textMoney;
-    private final String textSeveral;
-    private final String textTime;
-    private final String textToday;
-    private final String textExtend;
-    private final String textCharge;
-    private final String textCancel;
-    private final String textMkad;
+    private final String moneyTest;
+    private final String severalText;
+    private final String timeText;
+    private final String todayText;
+    private final String extendText;
+    private final String chargeText;
+    private final String cancelText;
+    private final String mkadText;
 
     // Создаем конструктор и инициализируем поля класса в нем
-    public AccordionTest(String textMoney, String textSeveral, String textTime, String textToday, String textExtend,
-                         String textCharge, String textCancel, String textMkad, int typeDriver) {
+    public AccordionTest(String moneyTest, String severalText, String timeText, String todayText, String extendText,
+                         String chargeText, String cancelText, String mkadText, int typeDriver) {
         super(typeDriver);
-        this.textMoney = textMoney;
-        this.textSeveral = textSeveral;
-        this.textTime = textTime;
-        this.textToday = textToday;
-        this.textExtend = textExtend;
-        this.textCharge = textCharge;
-        this.textCancel = textCancel;
-        this.textMkad = textMkad;
+        this.moneyTest = moneyTest;
+        this.severalText = severalText;
+        this.timeText = timeText;
+        this.todayText = todayText;
+        this.extendText = extendText;
+        this.chargeText = chargeText;
+        this.cancelText = cancelText;
+        this.mkadText = mkadText;
     }
 
     @Parameterized.Parameters // Аннотация метода для параметров
     public static Object[][] getText() {
-        String textMoney = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-        String textSeveral = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
-        String textTime = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
-        String textToday = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
-        String textExtend = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-        String textCharge = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
-        String textCancel = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
-        String textMkad = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+        String moneyTest = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        String severalText = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+        String timeText = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+        String todayText = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+        String extendText = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        String chargeText = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+        String cancelText = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        String mkadText = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
 
         return new Object[][]{
-                {textMoney, textSeveral, textTime, textToday, textExtend, textCharge, textCancel, textMkad, 0},
-                {textMoney, textSeveral, textTime, textToday, textExtend, textCharge, textCancel, textMkad, 1}
+                {moneyTest, severalText, timeText, todayText, extendText, chargeText, cancelText, mkadText, 0},
+                {moneyTest, severalText, timeText, todayText, extendText, chargeText, cancelText, mkadText, 1}
         };
     }
 
@@ -57,13 +57,13 @@ public class AccordionTest extends BasePageTest {
         mainPage.scrollDown(); //Скролл до вопросов о важном
 
         //сравниваем открывающийся текст в аккордионе
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionMoneyAnswer(), textMoney);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionSeveralAnswer(), textSeveral);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionTimeAnswer(), textTime);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionTodayAnswer(), textToday);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionExtendAnswer(), textExtend);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionChargeAnswer(), textCharge);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionCancelAnswer(), textCancel);
-        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionMkadAnswer(), textMkad);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionMoneyAnswer(), moneyTest);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionSeveralAnswer(), severalText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionTimeAnswer(), timeText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionTodayAnswer(), todayText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionExtendAnswer(), extendText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionChargeAnswer(), chargeText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionCancelAnswer(), cancelText);
+        Assert.assertEquals(getDriverNameMsg(), mainPage.getAccordionMkadAnswer(), mkadText);
     }
 }
